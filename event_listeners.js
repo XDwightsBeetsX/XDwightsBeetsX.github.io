2e0bd5e1410bd4781876abfff751bc89ba09df68
John Gutierrez <gutierrezjohn2000@gmail.com>
2021-07-18 02:55:07 -0500

    new design done

--- event_listeners.js
+++
@@ -1,9 +0,0 @@
-var sections = ["bio", "tws", "proj", "math"];
-
-function show(idToShow) {
-    // Goes through all the sections, which are class names in menu
-    // and makes all but the requested ClassToShow visible.
-    sections.forEach(section => document.getElementById(section).style.display='none');
-    
-    document.getElementById(idToShow).style.display='flex';
-}
